26fa78699b5e5c6b25dd7ba2270a527a82cfbc56
EntityFX <artem.solopiy@gmail.com>
2020-10-20 19:00:05 +0300

    Added intdiv polyfill

--- src/php/Writer.php
+++ src/php/Writer.php
@@ -76,3 +76,60 @@ namespace EntityFX\NetBenchmark\Core
 		}
 	}
 }
+
+namespace {
+    if (!defined('PHP_INT_MIN')) {
+        define('PHP_INT_MIN', ~PHP_INT_MAX);
+    }
+
+    if (!function_exists('intdiv')) {
+        function intdiv($dividend, $divisor)
+        {
+            $dividend = (int) $dividend;
+            $divisor = (int) $divisor;
+
+            if ($divisor === 0) {
+                throw new DivisionByZeroError('Division by zero');
+            }
+
+            if ($divisor === -1 && $dividend === PHP_INT_MIN) {
+                throw new ArithmeticError('Division of PHP_INT_MIN by -1 is not an integer');
+            }
+
+            $dividend = ($dividend - $dividend % $divisor);
+
+            return ((int) ($dividend / $divisor));
+        }
+    }
+
+    if ((!class_exists('ArithmeticError') || !class_exists('DivisionByZeroError')) 
+        && (!class_exists('Error') || (!in_array('Throwable', class_implements('Error')) && !in_array('Throwable', class_implements('Exception'))))) {
+        class Error extends \Exception
+        {
+            public function __toString()
+            {
+                return $this->getMessage();
+            }
+        }
+    }
+
+    if (!class_exists('ArithmeticError')) {
+        class ArithmeticError extends Error
+        {
+            public function __toString()
+            {
+                return $this->getMessage();
+            }
+        }
+    }
+
+    if (!class_exists('DivisionByZeroError')) {
+        class DivisionByZeroError extends Error
+        {
+            public function __toString()
+            {
+                return $this->getMessage();
+            }
+        }
+    }
+}
